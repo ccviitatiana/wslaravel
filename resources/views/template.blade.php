47327de72e3513f7ca4b9f97e873a9a7b3353f03
ccviitatiana <ffloowf@gmail.com>
2024-02-14 16:40:39 -0500

    commit5

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,15 +9,28 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Laravel</title>
 </head>
+
 <body>
-    <p>
-        <a href="{{ route('home')}}">Home</a>
-        @auth
-            <a href="{{ route('dashboard') }}">Dashboard</a>
-            @else
-            <a href="{{ route('login') }}">Login</a>
-        @endauth
-    </p>
+    <div class="container px-4 mx-auto">
+        <header class="flex justify-between items-center py-4">
+            <div class="flex items-center flex-grow gap-4">
+                <a href="{{ route('home') }}">
+                    <img class="h-12" src="{{ asset('build/assets/logo.png') }}" alt="logo">
+                </a>
+            </div>
+            <p>
+                <a href="{{ route('home') }}">Home</a>
+                @auth
+                    <a href="{{ route('dashboard') }}">Dashboard</a>
+                @else
+                    <a href="{{ route('login') }}">Login</a>
+                @endauth
+            </p>
+            <form action="" placeholder="Search..."></form>
+        </header>
+    </div>
+
     @yield('content')
 </body>
+
 </html>
