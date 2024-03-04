bbddf97abf9257511b5f866b72488efd05f9bdec
ccviitatiana <ffloowf@gmail.com>
2024-03-04 10:53:53 -0500

    url thing

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="icon" href="{{ asset('public/logo.png') }}">
+    <link rel="icon" href="{{ URL( '/icon.png' ) }}">
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
