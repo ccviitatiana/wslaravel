2b6883816d2610c10df72e087f92d8fa7f03ed0d
ccviitatiana <ffloowf@gmail.com>
2024-03-05 18:00:08 -0500

    toggle

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,25 +10,41 @@
 <div class="md:grid flex md:grid-cols-2 flex-col px-[5%] pb-20 pt-10 gap-20">
     <div class=''>
         <h1 class="text-2xl mb-8 text-gray-900">Registros</h1>
-        <div class='grid grid-cols-1 gap-4 pb-10 mb-4 justify-items-start items-center'>
+        <div class='pb-10 mb-4 justify-items-start items-center'>
             @foreach ($posts as $post)
-            <a href="{{ route('post', $post->slug) }}" class="bg-gray-200 rounded-lg px-6 pt-6 pb-3 w-full">
-                <p class="text-xs flex items-center gap-2">
-                    <span class="uppercase text-gray-200 bg-gray-900 rounded-full px-2 py-1">Tutorial</span>
-                    <span>{{ $post->created_at->format('d/m/Y') }}</span>
-                </p>
-                <h2 class="text-lg text-gray-900 mt-2 px-6 pt-4">{{ $post->title }}</h2>
-                <img src="{{ url('images/' . $post->image_path) }}" alt="">
-                <div class="bg-gray-200 rounded-lg flex items-center grow-2 justify-between flex-grow gap-8 bg-gray-200 px-6 py-4 w-full">
-                    <img id="reaction" onclick="reaction" src="{{ URL( '/reaction-off.png' ) }}" class="size-6" alt="">
+            <div class="mb-5">
+                <a href="{{ route('post', $post->slug) }}" class="">
+                    <div class="bg-gray-200 rounded-t-lg px-6 pt-6 pb-3 w-full">
+                        <p class="text-xs flex items-center gap-2">
+                            <span class="uppercase text-gray-200 bg-gray-900 rounded-full px-2 py-1">Tutorial</span>
+                            <span>{{ $post->created_at->format('d/m/Y') }}</span>
+                        </p>
+                        <h2 class="truncate text-lg text-gray-900 px-5 pt-4">{{ $post->title }}</h2>
+                        <img src="{{ url('images/' . $post->image_path) }}" alt="">
+                    </div>
+                </a>
+                <div class="bg-gray-200 rounded-b-lg flex items-center grow-2 justify-between flex-grow gap-8 bg-gray-200 px-6 pb-4 w-full">
+                    <img id="reaction" onclick="reaction" src="{{ URL( '/reaction-off.png' ) }}" class="ml-7 size-6" alt="">
+                    <script>
+                        const img = document.getElementById('reaction');
+                        let toggle = true;
+                        img.addEventListener('click', function() {
+                            toggle = !toggle;
+                            if(toggle) {
+                                img.src =
+                            }
+                        })
+                    </script>
                     <input class="border-none focus:ring-0 rounded-lg w-[420px]" type="text" placeholder="Comment...">
                 </div>
-            </a>
+            </div>
             @endforeach
         </div>
         {{ $posts->links() }}
+
     </div>
-    
+
+
     <div class=''>
         defdwerewf
     </div>
